61282048e379191e58520c3cb53930f380388c99
Eduardo Lucio <eduardomalone@hotmail.com>
2023-05-26 01:08:48 -0300

    criacao do projeto

---
+++ backend/src/main/java/com/epi/epideliver/EpideliverApplication.java
@@ -0,0 +1,13 @@
+package com.epi.epideliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpideliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpideliverApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/epi/epideliver/EpideliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epi.epideliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpideliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
