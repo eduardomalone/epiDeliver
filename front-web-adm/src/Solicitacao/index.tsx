593d2a079efd2a9dbd7ec9df7bda3297e5a3973d
eduardomalone <eduardomalone@hotmail.com>
2024-10-13 00:25:19 -0300

    tentando 40

--- front-web-adm/src/Solicitacao/index.tsx
+++ front-web-adm/src/Solicitacao/index.tsx
@@ -155,9 +155,9 @@ function Solicitacoes() {
             await new Promise(r => setTimeout(r, 500));
             if (valCodBar !== '') i = 10;
                 aoMudarTextoDeBusca(valCodBar[1].substring(0, valCodBar[1].length - 1));
-                setTextoDaBusca(valCodBar[1].substring(0, valCodBar[1].length - 1));
-                alert(`valCodBar in if `+valCodBar)
-            alert(`valCodBar out if `+valCodBar)
+                //setTextoDaBusca(valCodBar[1].substring(0, valCodBar[1].length - 1));
+                alert(`valCodBar in if: `+valCodBar)
+            alert(`valCodBar out if: `+valCodBar)
         }
         
     }
