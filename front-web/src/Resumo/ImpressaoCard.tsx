6dc0a6f73ef9b2c8c1162a6f84062036b0944ba1
eduardomalone <eduardomalone@hotmail.com>
2024-10-13 17:35:48 -0300

    13

--- front-web/src/Resumo/ImpressaoCard.tsx
+++ front-web/src/Resumo/ImpressaoCard.tsx
@@ -16,35 +16,32 @@ function ImpressaoCard({ product, registro, codBarras }: Props) {
 
   return (
     <>
-      {/* <Card>
-          <CardContent>
-          </CardContent>
-     </Card>      */}
 
-
-      {/* <Card>
-        <CardContent> */}
-      <Box padding={8} display='flex' alignItems='center' justifyItems='center' >
-
-        <h6 className="order-card-title-teste">
-          {product.descricao}
-        </h6>
-        <h1 >
-
-          <Barcode value={codBarras}
-          //format={"CODE128"}
-          //format={"CODE12"}
-          // height={50}
-          // width={1}
-          // textAlign= {"center"}
-          // textPosition= {"bottom"}
-          //textMargin= {2}
-          // fontSize={5}
-          //marginRight= {20}
-          // marginLeft={0}
-          />
-        </h1>
-
+      <Box padding={5} display='flex' alignItems='center' justifyItems='center' >
+        <div className="">
+          <div className="">
+            {/* <h6 className="order-card-title-teste">
+              {product.descricao}
+            </h6> */}
+            <h1 >
+              <Barcode value={codBarras}
+              //format={"CODE128"}
+              //format={"CODE12"}
+              // height={50}
+              // width={1}
+              // textAlign= {"center"}
+              // textPosition= {"bottom"}
+              //textMargin= {2}
+              // fontSize={5}
+              //marginRight= {20}
+              // marginLeft={0}
+              />
+              <h6 className="order-card-title-teste">
+              {product.descricao}
+            </h6>
+            </h1>
+          </div>
+        </div>
 
       </Box>
     </>
